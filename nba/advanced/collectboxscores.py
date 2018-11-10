d451997097fb497f70363d6634f2ccde8b4f6cf1
Laimonas Simutis <lsimutis@mediafly.com>
2018-11-10 14:01:15 -0600

    finish scraper for box scores

---
+++ nba/advanced/collectboxscores.py
@@ -0,0 +1,27 @@
+import scraper
+import database
+import datetime
+
+if __name__ == "__main__":
+
+	dt = datetime.date(2016,10,25)
+	end = datetime.date(2017,4,10)
+
+	while dt < end:
+		
+		print(datetime.datetime.now(),"processing",dt)
+
+		links = scraper.get_boxscore_links(dt.year, dt.month, dt.day)
+
+		print("    ",datetime.datetime.now(),"received ",len(links))
+
+		for l in links:
+			print("    ",datetime.datetime.now(),"getting ",l)
+
+			game = scraper.get_boxscore_details(dt.year, dt.month, dt.day, l)
+
+			print("    ",datetime.datetime.now(),"saving ",game)
+
+			database.insert_box_score(game)
+
+		dt = dt + datetime.timedelta(days=1)
---
+++ nba/advanced/script.sql
@@ -0,0 +1,21 @@
+DROP SEQUENCE games_id CASCADE;
+CREATE SEQUENCE games_id;
+ALTER TABLE games_id OWNER TO bet;
+
+DROP TABLE games;
+CREATE TABLE games (id text DEFAULT(nextval('games_id')) PRIMARY KEY, date timestamp, away text, home text);
+ALTER TABLE games owner TO bet;
+
+CREATE UNIQUE INDEX games_date_away_home_idx ON games USING btree(date, away, home);
+
+DROP SEQUENCE gamerows_id CASCADE;
+CREATE SEQUENCE gamerows_id;
+ALTER TABLE gamerows_id OWNER TO bet;
+
+DROP TABLE gamerows;
+CREATE TABLE gamerows(
+	id text DEFAULT(nextval('gamerows_id')) PRIMARY KEY, 
+	gameid TEXT, team text, name text, 
+	minutes text, fgm int, fga int, tpm int, tpa int, ftm int, fta int, oreb int, dreb int, assists int, steals int, blocks int, turnovers int, fouls int, points int);
+ALTER TABLE gamerows owner TO bet;
+
