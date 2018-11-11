cf85f140079ea08e5bbb80bc7c0e9adb313ed466
Laimonas Simutis <lsimutis@mediafly.com>
2018-11-11 12:56:26 -0600

    reorder locations of scripts

--- nba/test_importer.py
+++ nba/test_importer.py
@@ -19,14 +19,14 @@ class TestImport(unittest.TestCase):
 	def test_first_row_correct(self):
 
 		self.assertEqual(
-			[2015, '2015-11-09', 100, 'Portland Trail Blazers', 'Denver Nuggets', 1, 0.5714285714285714, 0.3333333333333333, 1.7142857142857082, -6.166666666666671],
+			[2015, '2015-11-09', 100, 'por', 'den', 1, 0.5714285714285714, 0.3333333333333333, 1.7142857142857082, -6.166666666666671, 0.4503296703296703, 0.2778125928125928],
 			self.output[0]
 		)
 
 	def test_last_row_correct(self):
 
 		self.assertEqual(
-			[2015, '2016-04-09', 1193, 'Oklahoma City Thunder', 'Sacramento Kings', 1, 0.7, 0.4, 7.300000000000011, 1.2000000000000028],
+			[2015, '2016-04-09', 1193, 'okc', 'sac', 1, 0.7, 0.4, 7.300000000000011, 1.2000000000000028, 0.34682245386192745, 0.39065267671251447],
 			self.output[len(self.output)-1]
 		)
 
@@ -53,10 +53,10 @@ class TestImport(unittest.TestCase):
 
 	def test_specific_stats_match(self):
 
-		team = "Chicago Bulls"
+		team = "chi"
 
 		verify = {
-			"wins": 40, "scored": 8099, "allowed": 8235, "date": 0
+			"wins": 40, "scored": 8099, "allowed": 8235, "date": 0, "ftm": 35.30051187574503, "tpm": 30.90915325347887
 		}
 
 		team_stats = self.stats[team]
