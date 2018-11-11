5401398c46c1204abea9cd7269ee59f380504c10
Laimonas Simutis <lsimutis@mediafly.com>
2018-11-11 06:47:42 -0600

    rename to make sure it's clear what is what

---
+++ nba/advanced/run_generateinputs.py
@@ -0,0 +1,13 @@
+import database
+import datetime
+
+import csv
+
+if __name__ == "__main__":
+	
+	year = 2015
+
+	start = datetime.date(2015,10,1)
+	end = datetime.date(2016,4,10)
+
+	# rows = database.get_game_stats()
