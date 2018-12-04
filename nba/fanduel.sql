47d1cc41eefe4c7fb83b18e86e996924956020db
Laimonas Simutis <lsimutis@mediafly.com>
2018-12-04 13:43:23 -0600

    latest updates

--- nba/fanduel.sql
+++ nba/fanduel.sql
@@ -2,17 +2,13 @@
 join games g on g.id = gr.gameid
 where g.date > current_date - 20
 and team in (
-'cle',
-'tor',
-'det',
-'gsw',
-'hou',
-'chi',
-'min',
-'bos',
-'sac',
-'ind',
-'was',
-'brk')
+'dal',
+'lac',
+'mia',
+'uta',
+'phi',
+'mem',
+'sas',
+'por')
 group by team, name
 order by sum(fgm * 2) + sum(tpm * 3) + sum(ftm) + sum((oreb+dreb)*1.2) + sum(assists * 1.5) + sum(blocks * 3) + sum(steals * 3) + sum(turnovers * -1) desc
